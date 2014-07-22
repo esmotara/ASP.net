bfd7c73b1b23d5ad369354c4b3da0197241bd006
esmotara <esmo.ice@gmail.com>
2014-07-22 13:44:31 +0600

    PersonAPP practice

---
+++ PersonApp/PersonApp/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApp
+{
+    class Person
+    {
+        public string firstName;
+        public string middleName;
+        public string lastName;
+
+
+        public string FirstName
+        {
+            get { return firstName; }
+            
+        }
+
+        public string MiddleName
+        {
+            get { return middleName; }
+           
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+       
+        }
+
+      
+
+        public Person(string firstName, string middleName, string lastName)
+        {
+            this.firstName = firstName;
+            this.middleName = middleName;
+            this.lastName = lastName;
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public Person()
+        {
+         
+         }
+
+        public string GetFullName()
+        {
+            return firstName + " " + middleName + " " + lastName;
+        }
+
+        public string GetReverseName()
+        {
+            string fullName = GetFullName();
+            char[] cArray = fullName.ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+            return reverse;
+        }
+    }
+}
---
+++ PersonApp/PersonApp/PersonUI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonApp
+{
+    public partial class PersonUI : Form
+    {
+        private Person aPerson; 
+        public PersonUI()
+        {
+            InitializeComponent();
+        }
+
+        private void showFullNameButton_Click(object sender, EventArgs e)
+        {
+           
+            SetNames();
+            MessageBox.Show(aPerson.GetFullName());
+        }
+
+        private void SetNames()
+        {
+            aPerson = new Person(firstNameTextBox.Text, middleNameTextBox.Text, lastNameTextBox.Text);
+
+        }
+
+        private void ameButton_Click(object sender, EventArgs e)
+        {
+            SetNames();
+            MessageBox.Show(aPerson.GetReverseName());
+        }
+    }
+}
